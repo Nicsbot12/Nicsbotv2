b585fdb96dd6a9a9f6a6aaa29897d5eb28aea164
Nicsbot12 <154717275+Nicsbot12@users.noreply.github.com>
2024-08-21 07:56:09 +0800

    Update Ai.js

--- scripts/cmds/Ai.js
+++ scripts/cmds/Ai.js
@@ -1,13 +1,69 @@
+const axios = require("axios");
 module.exports = {
-config: {
-name: "ai", 
-author: "Aljur Pogoy",
-category: "Ai" 
-},
-onStart: () => {},
-onChat: async function ({ message: { reply: r }, args: a, event: { body } })  {
-if(!body?.toLowerCase().startsWith("ai"))
-return;
-require("axios").get(`${String.fromCharCode(104,116,116,112,115,58,47,47,116,111,111,108,115,46,98,101,116,97,98,111,116,122,46,101,117,46,111,114,103,47,116,111,111,108,115,47,111,112,101,110,97,105,63,113,61)}${a.slice(1).join(" ") || "hello"}`).then(({data:{result:_}})=>r(_)).catch(({message:_})=>r(_));
-}
+	config: {
+		name: 'ai',
+		version: '2.1.0',
+		author: 'KENLIEPLAYS',
+		countDown: 5,
+		role: 0,
+		shortDescription: 'AI by Kenlie Navacilla Jugarap',
+		longDescription: {
+			en: 'AI by Kenlie Navacilla Jugarap'
+		},
+		category: 'ai',
+		guide: {
+			en: '   {pn} <word>: ask with AI'
+				+ '\n   Example:{pn} hi'
+		}
+	}, 
+
+	langs: {
+		en: {
+			chatting: 'Please wait...',
+			error: 'If this report spam please contact Nico Adajar'
+		}
+	},
+
+	onStart: async function ({ args, message, event, getLang }) {
+		if (args[0]) {
+			const yourMessage = args.join(" ");
+			try {
+				const responseMessage = await getMessage(yourMessage);
+				return message.reply(`${responseMessage}`);
+			}
+			catch (err) {
+				console.log(err)
+				return message.reply(getLang("error"));
+			}
+		}
+	},
+
+	onChat: async ({ args, message, threadsData, event, isUserCallCommand, getLang }) => {
+		if (!isUserCallCommand) {
+			return;
+		}
+		if (args.length > 1) {
+			try {
+				const langCode = await threadsData.get(event.threadID, "settings.lang") || global.GoatBot.config.language;
+				const responseMessage = await getMessage(args.join(" "), langCode);
+				return message.reply(`${responseMessage}`);
+			}
+			catch (err) {
+				return message.reply(getLang("error"));
+			}
+		}
+	}
 };
+
+async function getMessage(yourMessage, langCode) {
+	try {
+		const res = await axios.get(`https://api.kenliejugarap.com/ai/?text=${yourMessage}`);
+		if (!res.data.response) {
+			throw new Error('Please contact Nico Adajar if this error spams...');
+		}
+		return res.data.response;
+	} catch (err) {
+		console.error('Error while getting a message:', err);
+		throw err;
+	}
+  }
